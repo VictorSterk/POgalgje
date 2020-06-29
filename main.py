2fc60f8387c877514fa37861ba7ca55360a84166
Victor Sterk <110495@st-maartenscollege.nl>
2020-06-29 13:39:32 +0000

    woordlijst toegevoegd

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
 print ('je hebt 5 beurten om het juiste woord te raden')
+import random
+Woordlijst = ['informatica', 'informatiekunde', 'spelletje', 'aardigheidje', 'scholier', 'fotografie','waardebepaling', 'specialiteit', 'verzekering', 'universiteit', 'heesterperk' ]
