1d5a42db379835462c6dfe2a64a4d62d08be5692
Victor Sterk <110495@st-maartenscollege.nl>
2020-06-29 13:56:29 +0000

    aantal letters aan het begin

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 print ('je hebt 5 beurten om het juiste woord te raden')
 import random
 Woordlijst = ['informatica', 'informatiekunde', 'spelletje', 'aardigheidje', 'scholier', 'fotografie','waardebepaling', 'specialiteit', 'verzekering', 'universiteit', 'heesterperk' ]
+GeheimWoord = Woordlijst[random.randrange(0, len(Woordlijst))]
+GeradenLetters = []
+
+def renderWoord():
+        for a in range(len(GeheimWoord)):
+            if GeheimWoord[a] in GeradenLetters:
+                print(GeheimWoord[a], '', end='')
+            else:
+                print('?', end='')
+        print('')
+        print('-' * len(GeheimWoord))
+
+renderWoord()
