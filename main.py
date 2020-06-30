a927413d886bafb33f1f3aba5f11c318d558ff22
Victor Sterk <110495@st-maartenscollege.nl>
2020-06-30 12:51:21 +0000

    puntjes op de i en het basisspel afgemaakt

--- main.py
+++ main.py
@@ -1,49 +1,56 @@
-print ('je hebt 5 beurten om het juiste woord te raden')
+print('je hebt 5 beurten om het juiste woord te raden')
 import random
-Woordlijst = ['informatica', 'informatiekunde', 'spelletje', 'aardigheidje', 'scholier', 'fotografie','waardebepaling', 'specialiteit', 'verzekering', 'universiteit', 'heesterperk' ]
-GeheimWoord = Woordlijst[random.randrange(0, len(Woordlijst))]
+WoordLijst = ['informatica', 'informatiekunde', 'spelletje', 'aardigheidje', 'scholier', 'fotografie','waardebepaling', 'specialiteit', 'verzekering', 'universiteit', 'heesterperk']
+GeheimWoord = WoordLijst[random.randrange(0, len(WoordLijst))]
 GeradenLetters = []
 beurten = 5
+
+def checkWin():
+    for i in range(len(GeheimWoord)):
+        if not GeheimWoord[i] in GeradenLetters:
+            return(False)
+    return(True)
 
 def renderWoord():
         for a in range(len(GeheimWoord)):
             if GeheimWoord[a] in GeradenLetters:
                 print(GeheimWoord[a], '', end='')
             else:
-                print('?', end='')
+                print('? ', end='')
         print('')
         print('-' * len(GeheimWoord))
 
 renderWoord()
 
 while True:
-    letter = input('raad een letter of het hele woord  ')
+    letter = input('raad een letter of het woord  ')
     if len(letter) == 1:
+        if letter in GeradenLetters:
+            print('Die letter had je al geraden')
             renderWoord()
-
+        else:
             GeradenLetters += letter
             if letter in GeheimWoord:
-                print('Die letter zit in het woord')
-                if checkWin():
+                print('Die letter zit wel in het woord')
                 renderWoord()
                 if checkWin():
                     print('Je hebt alle letters geraden')
                     print('Het woord was: ' + GeheimWoord)
                     break
-
- else:
-    GeradenLetters += letter
-    if letter in GeheimWoord:
-                print('Die letter zit in het woord')
-                renderWoord()
-
-   else:
-      print('Die letter zit niet in het woord')
+            else:
+                print('Die letter zit niet in het woord')
                 print('-1')
                 beurten -= 1
                 renderWoord()
-
-if beurten <= 0:
-        print('Je hebt verlore. Het woord was ' + GeheimWoord)
+    elif letter == GeheimWoord:
+        print('Je hebt het woord geraden')
         break
-
+    else:
+        print('Fout woord!')
+        print('-2')
+        beurten -= 2
+        renderWoord()
+    if beurten <= 0:
+        print('Je hebt verloren! Het woord was ' + GeheimWoord)
+        break
+    print('Je hebt nog', beurten, 'beurten over.')
